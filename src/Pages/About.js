1f88e4cd416d871494ca01365f55526febbaa54e
Manuel <tofuah@gmail.com>
2021-09-22 12:38:22 +0100

    added function,h1 and paragraph

---
+++ src/Pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat
+        metus lacus. Donec quis justo non dui placerat mattis. Mauris et sapien
+        id leo porta eleifend ac quis turpis. Aenean eu convallis lorem. In
+        viverra urna non mi faucibus, vel laoreet justo semper. Orci varius
+        natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+        mus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+      </p>
+    </div>
+  );
+}
