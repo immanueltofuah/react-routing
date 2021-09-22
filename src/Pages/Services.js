1a6cc6ac99e4d3d3c2b209525d1fcaee19eb810e
Manuel <tofuah@gmail.com>
2021-09-22 14:44:17 +0100

    changed home to services

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Services() {
   return (
     <div>
-      <h1>Home Page </h1>
+      <h1>Services Page </h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin feugiat
         metus lacus. Donec quis justo non dui placerat mattis. Mauris et sapien
@@ -12,6 +13,7 @@ export default function Services() {
         natoque penatibus et magnis dis parturient montes, nascetur ridiculus
         mus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
       </p>
+      <Link to="/Services/:201">goto sd</Link>
     </div>
   );
 }
