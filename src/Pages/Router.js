4485094c1eb4eaba0d39e70ebd9ff4a818169f3a
Manuel <tofuah@gmail.com>
2021-09-22 13:43:38 +0100

    formated document using prettier code

--- src/Pages/Router.js
+++ src/Pages/Router.js
@@ -1,30 +1,24 @@
-
-import {BrowserRouter , Route, Link} from 'react-router-dom'
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import Home from "./Home";
-import About from './About';
-import Contact from './Contact';
-import Services from './Services';
+import About from "./About";
+import Contact from "./Contact";
+import Services from "./Services";
 
- function Router() {
-    return (
-        
-            <BrowserRouter>
-            
-            <Link to="/">Go to Home</Link>
-            <Link to="/About">Go to About</Link>
-            <Link to="/Contact">Go to Contact</Link>
-            <Link to="/Services">Go to Services</Link>
+function Router() {
+  return (
+    <BrowserRouter>
+      <Link to="/">Go to Home</Link>
+      <Link to="/About">Go to About</Link>
+      <Link to="/Contact">Go to Contact</Link>
+      <Link to="/Services">Go to Services</Link>
 
-            <Route exact="true" path="/" component={Home}/>
-            <Route  path="/About" component={About}/>
-            <Route  path="/Contact" component={Contact}/>
-            <Route  path="/Services" component={Services}/>
-
-            </BrowserRouter>
-            
-      
-    )
+      <Route exact="true" path="/" component={Home} />
+      <Route path="/About" component={About} />
+      <Route path="/Contact" component={Contact} />
+      <Route path="/Services" component={Services} />
+    </BrowserRouter>
+  );
 }
 
 export default Router;
